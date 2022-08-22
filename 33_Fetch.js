67ff787b598f70a862956b4ed8035449796b2457
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-08-22 19:59:52 +0530

    Fetch

---
+++ 33_Fetch.js
@@ -0,0 +1,19 @@
+const URL = "https://jsonplaceholder.typicode.com/users";
+fetch(URL)
+  .then((response) => {
+    return response.json();
+  })
+  .them((data) => {
+    console.log(data.map((user) => user.name));
+    // console.log(data)
+  });
+
+// ABOVE CODE USING ASYNC
+
+async function doStuff() {
+  const response = await fetch(URL);
+  const users = await response.data;
+  console.log(users);
+}
+
+doStuff();
